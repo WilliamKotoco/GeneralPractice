ad7a7e9dc35838b42d4d4bd381db268b60cffafd
William Martins <williamdabruzzmartins76136@gmail.com>
2024-07-04 22:09:53 -0300

    new cuda code

--- cuda/matrix_add.cu
+++ cuda/matrix_add.cu
@@ -1,105 +1,65 @@
-#include <__clang_cuda_builtin_vars.h>
-#include <cstdlib>
 #include <cuda.h>
 #include <stdio.h>
 #include <stdlib.h>
+#define N 100
 
-#define N 1000
+#define CUDA_CHECK_RETURN(value)                                               \
+  {                                                                            \
+    cudaError_t _m_cudaStat = value;                                           \
+    if (_m_cudaStat != cudaSuccess) {                                          \
+      fprintf(stderr, "Error %s at line %d in file %s\n",                      \
+              cudaGetErrorString(_m_cudaStat), __LINE__, __FILE__);            \
+      exit(1);                                                                 \
+    }                                                                          \
+  }
 
-// Error checking macro
-#define CUDA_CHECK_RETURN(value)                                                                                       \
-    {                                                                                                                  \
-        cudaError_t _m_cudaStat = value;                                                                               \
-        if (_m_cudaStat != cudaSuccess)                                                                                \
-        {                                                                                                              \
-            fprintf(stderr, "Error %s at line %d in file %s\n", cudaGetErrorString(_m_cudaStat), __LINE__, __FILE__);  \
-            exit(1);                                                                                                   \
-        }                                                                                                              \
+__global__ void add_matrix_kernal(float *d_a, float *d_b, float *d_c) {
+  int row = blockIdx.y * blockDim.y + threadIdx.y;
+  int column = blockIdx.x * blockDim.x + threadIdx.x;
+
+  if (row < N && column < N)
+    d_c[row * N + column] = d_a[row * N + column] + d_b[row * N + column];
+}
+
+void add_matrix(float *h_a, float *h_b, float *h_c) {
+  float *d_a;
+  float *d_b;
+  float *d_c;
+
+  CUDA_CHECK_RETURN(cudaMalloc(&d_a, N * N * sizeof(float)));
+  CUDA_CHECK_RETURN(cudaMalloc(&d_b, N * N * sizeof(float)));
+  CUDA_CHECK_RETURN(cudaMalloc(&d_c, N * N * sizeof(float)));
+
+  cudaMemcpy(d_a, h_a, N * N * sizeof(float), cudaMemcpyHostToDevice);
+  cudaMemcpy(d_b, h_b, N * N * sizeof(float), cudaMemcpyHostToDevice);
+
+  dim3 threads_per_block(16, 16);
+  dim3 blocks_per_grid((N + threads_per_block.x - 1) / threads_per_block.x,
+                       (N + threads_per_block.y - 1) / threads_per_block.y);
+
+  add_matrix_kernal<<<blocks_per_grid, threads_per_block>>>(d_a, d_b, d_c);
+
+  cudaMemcpy(h_c, d_c, N * N * sizeof(float), cudaMemcpyDeviceToHost);
+
+  cudaFree(d_a);
+  cudaFree(d_b);
+  cudaFree(d_c);
+}
+int main() {
+  float *h_a = (float *)malloc((N * N) * sizeof(float));
+  float *h_b = (float *)malloc((N * N) * sizeof(float));
+  float *h_c = (float *)malloc((N * N) * sizeof(float));
+
+  for (int i = 0; i < N; i++) {
+    for (int j = 0; j < N; j++) {
+      h_a[i * N + j] = (float)rand() / (RAND_MAX / 100.0);
+      h_b[i * N + j] = (float)rand() / (RAND_MAX / 100.0);
     }
+  }
 
-/// each thread produces one output matrix element
-__device__ void add_matrix_kernal_1(float **A, float **B, float **C)
-{
-    int row = threadIdx.x + blockDim.x * blockIdx.x;
+  add_matrix(h_a, h_b, h_c);
 
-}
+  printf("%f + %f = %f", h_a[0], h_b[0], h_c[0]);
 
-/// each thread produces one output matrix row
-__device__ void add_matrix_kernal_3(float **A, float **B, float **C)
-{
-    int row = threadIdx.x + blockDim.x * blockIdx.x;
-    
-    int size = blockDim.x > N ? N : blockDim.x;
-
-    for (int i = 0 ; i < size; i++)
-    {
-      C[row][i] = A[row][i] + B[row][i];
-    }
-}
-
-/// each thread produces one output matrix column
-__device__ void add_matrix_kernal_2(float **A, float **B, float **C)
-{
-    return;
-}
-
-/// not necessary the __host__ once it is by default
-__host__ float **matrix_alloc()
-{
-    float **matrix = (float **)malloc(N * sizeof(float *));
-
-    for (int i = 0; i < N; i++)
-    {
-        matrix[i] = (float *)malloc(N * sizeof(float));
-    }
-
-    return matrix;
-}
-
-float **matrix_cuda_malloc()
-{
-    float **matrix;
-    CUDA_CHECK_RETURN(cudaMalloc((void **)&matrix, N * sizeof(float *)));
-
-    for (int i = 0; i < N; i++)
-    {
-        CUDA_CHECK_RETURN(cudaMalloc((void **)&matrix[i], N * sizeof(float)));
-    }
-
-    return matrix;
-}
-
-void matrix_add(float **h_A, float **h_B, float **h_C)
-{
-    float **d_A = matrix_cuda_malloc();
-    float **d_B = matrix_cuda_malloc();
-    float **d_C = matrix_cuda_malloc();
-
-    cudaMemcpy(d_A, h_A, N * sizeof(float *), cudaMemcpyKind::cudaMemcpyHostToDevice);
-    cudaMemcpy(d_B, h_B, N * sizeof(float *), cudaMemcpyKind::cudaMemcpyHostToDevice);
-
-
-
-    cudaMemcpy(h_C, d_C, N * sizeof(float *), cudaMemcpyKind::cudaMemcpyDeviceToHost);
-    cudaFree(d_A);
-    cudaFree(d_B);
-    cudaFree(d_C);
-}
-int main()
-{
-    float **h_A = matrix_alloc();
-    float **h_B = matrix_alloc();
-    float **h_C = matrix_alloc();
-
-    for (int i = 0; i < N; i++)
-    {
-        for (int j = 0; j < N; j++)
-        {
-            h_A[i][j] = (float)rand() / 1000;
-            h_B[i][j] = (float)rand() / 1000;
-        }
-    }
-
-    matrix_add(h_A, h_B, h_C);
-    return 0;
+  return 0;
 }
--- cuda/vector_add.cu
+++ cuda/vector_add.cu
@@ -5,75 +5,70 @@
 
 #define N 10000
 // Error checking macro
-#define CUDA_CHECK_RETURN(value)                                                                                       \
-    {                                                                                                                  \
-        cudaError_t _m_cudaStat = value;                                                                               \
-        if (_m_cudaStat != cudaSuccess)                                                                                \
-        {                                                                                                              \
-            fprintf(stderr, "Error %s at line %d in file %s\n", cudaGetErrorString(_m_cudaStat), __LINE__, __FILE__);  \
-            exit(1);                                                                                                   \
-        }                                                                                                              \
-    }
+#define CUDA_CHECK_RETURN(value)                                               \
+  {                                                                            \
+    cudaError_t _m_cudaStat = value;                                           \
+    if (_m_cudaStat != cudaSuccess) {                                          \
+      fprintf(stderr, "Error %s at line %d in file %s\n",                      \
+              cudaGetErrorString(_m_cudaStat), __LINE__, __FILE__);            \
+      exit(1);                                                                 \
+    }                                                                          \
+  }
 
 /// there is no need for loops, each thread corresponds to an interation
-__global__ void vec_add_kernel(float *A, float *B, float *C)
-{
+__global__ void vec_add_kernel(float *A, float *B, float *C) {
 
-    /// size of block * the id identifies unically the index of the original
-    /// vector
-    int index = threadIdx.x + (blockDim.x * blockIdx.x);
+  /// size of block * the id identifies unically the index of the original
+  /// vector
+  int index = threadIdx.x + (blockDim.x * blockIdx.x);
 
-    /// the if statemant is necessary since the size of the vector may not match
-    /// the block size, once the block size is multiple of 32. It is necessary to
-    /// discard the remaining threads
-    if (index < N)
-        C[index] = A[index] + B[index];
+  /// the if statemant is necessary since the size of the vector may not match
+  /// the block size, once the block size is multiple of 32. It is necessary to
+  /// discard the remaining threads
+  if (index < N)
+    C[index] = A[index] + B[index];
 }
-void vec_add(float *h_a, float *h_b, float *h_c)
-{
+void vec_add(float *h_a, float *h_b, float *h_c) {
 
-    /// declaring the device arrays
-    float *d_a;
-    float *d_b;
-    float *d_c;
+  /// declaring the device arrays
+  float *d_a;
+  float *d_b;
+  float *d_c;
 
-    /// allocating memory in device
-    CUDA_CHECK_RETURN(cudaMalloc((void **)&d_a, N * sizeof(float)));
-    CUDA_CHECK_RETURN(cudaMalloc((void **)&d_b, N * sizeof(float)));
-    CUDA_CHECK_RETURN(cudaMalloc((void **)&d_c, N * sizeof(float)));
+  /// allocating memory in device
+  CUDA_CHECK_RETURN(cudaMalloc((void **)&d_a, N * sizeof(float)));
+  CUDA_CHECK_RETURN(cudaMalloc((void **)&d_b, N * sizeof(float)));
+  CUDA_CHECK_RETURN(cudaMalloc((void **)&d_c, N * sizeof(float)));
 
-    /// copying from host to device
-    cudaMemcpy(d_a, h_a, N * sizeof(float), cudaMemcpyHostToDevice);
-    cudaMemcpy(d_b, h_b, N * sizeof(float), cudaMemcpyHostToDevice);
+  /// copying from host to device
+  cudaMemcpy(d_a, h_a, N * sizeof(float), cudaMemcpyHostToDevice);
+  cudaMemcpy(d_b, h_b, N * sizeof(float), cudaMemcpyHostToDevice);
 
-    /// number of blocks and number of threads within a block
-    vec_add_kernel<<< ceil(N/ 256.0), 256>>>(d_a, d_b, d_c);
+  /// number of blocks and number of threads within a block
+  vec_add_kernel<<<ceil(N / 256.0), 256>>>(d_a, d_b, d_c);
 
-    /// copying from device to host after performing the vector addition i
-    cudaMemcpy(h_c, d_c, N * sizeof(float), cudaMemcpyDeviceToHost);
+  /// copying from device to host after performing the vector addition i
+  cudaMemcpy(h_c, d_c, N * sizeof(float), cudaMemcpyDeviceToHost);
 
-
-    printf("teste %f \n", h_c[0]);
-    /// free device's memory
-    cudaFree(d_a);
-    cudaFree(d_b);
-    cudaFree(d_c);
+  printf("teste %f \n", h_c[0]);
+  /// free device's memory
+  cudaFree(d_a);
+  cudaFree(d_b);
+  cudaFree(d_c);
 }
-int main()
-{
-    float *h_a = (float *)malloc(N * sizeof(float));
-    float *h_b = (float *)malloc(N * sizeof(float));
-    float *h_c = (float *)malloc(N * sizeof(float));
+int main() {
+  float *h_a = (float *)malloc(N * sizeof(float));
+  float *h_b = (float *)malloc(N * sizeof(float));
+  float *h_c = (float *)malloc(N * sizeof(float));
 
-    for (int i = 0; i < N; i++)
-    {
-        h_a[i] = (float)rand() / (RAND_MAX/100.0);
-        h_b[i] = (float)rand() / (RAND_MAX / 100.0);
-    }
+  for (int i = 0; i < N; i++) {
+    h_a[i] = (float)rand() / (RAND_MAX / 100.0);
+    h_b[i] = (float)rand() / (RAND_MAX / 100.0);
+  }
 
-    vec_add(h_a, h_b, h_c);
+  vec_add(h_a, h_b, h_c);
 
-    printf("Test: %f + %f = %f", h_a[0], h_b[0], h_c[0]);
+  printf("Test: %f + %f = %f", h_a[0], h_b[0], h_c[0]);
 
-    return 0;
+  return 0;
 }
